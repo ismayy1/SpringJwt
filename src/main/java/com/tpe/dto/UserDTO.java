0d3801d3eba7b21d5f84f0081d4061a166440a0b
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2025-01-25 18:04:27 +0200

    Spring JWT securty

---
+++ src/main/java/com/tpe/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package com.tpe.dto;
+
+import javax.persistence.Column;
+
+public class UserDTO {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String userName;
+}
