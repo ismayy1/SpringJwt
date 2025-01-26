ccaf6b8b49496900d53d3cd6838096269f58efb5
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2025-01-26 23:23:28 +0200

    Spring JWT securty

--- src/main/java/com/tpe/dto/UserDTO.java
+++ src/main/java/com/tpe/dto/UserDTO.java
@@ -1,12 +1,25 @@
 package com.tpe.dto;
 
-import javax.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
 
+    @NotBlank(message = "The firstName can't be null!")
     private String firstName;
 
+    @NotBlank(message = "The lastName can't be null!")
     private String lastName;
 
+    @NotBlank(message = "The userName can't be null!")
     private String userName;
 }
